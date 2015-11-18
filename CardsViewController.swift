1057845ce0ed75683255d56864b60a991fc6a9df
Mark Rabins <markrabins@gmail.com>
2015-11-17 16:52:53 -0800

    Create card view, intial view implementation

---
+++ CardsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CardsViewController.swift
+//  DateMatch
+//
+//  Created by Mark Rabins on 11/17/15.
+//  Copyright © 2015 self.swift. All rights reserved.
+//
+
+import UIKit
+
+weak var cardStackView: UIView!
+
+class CardsViewController: UIViewController {
+    
+    let frontCardTopMargin: CGFloat = 0
+    let backCardTopMargin: CGFloat = 10
+    
+    @IBOutlet weak var cardStackView: UIView!
+    
+    var backCard: SwipeView?
+    var frontCard: SwipeView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        backCard = SwipeView(frame: createCardFrame(backCardTopMargin))
+        cardStackView.addSubview(backCard!)
+        
+        frontCard = SwipeView(frame: createCardFrame(frontCardTopMargin))
+        cardStackView.addSubview(frontCard!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func createCardFrame(topMargin: CGFloat) -> CGRect {
+        return CGRect(x:0, y: topMargin, width: cardStackView.frame.width, height: cardStackView.frame.height)
+    }
+
+}
