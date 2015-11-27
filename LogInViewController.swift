4cc48e8dd365c0d4f5377b315c9623e512b7d025
Mark Rabins <mark.rabins@ensighten.com>
2015-11-27 13:35:00 -0800

    Add loginViewController

---
+++ LogInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LogInViewController.swift
+//  DateMatch
+//
+//  Created by Mark Rabins on 11/27/15.
+//  Copyright © 2015 self.swift. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pressedFBLogin(sender: UIButton) {
+        
+    }
+    
+
+}
