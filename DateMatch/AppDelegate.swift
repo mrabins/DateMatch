1dbf08f299692fef388cbcf044f88225880685b6
Mark Rabins <markrabins@gmail.com>
2015-11-28 23:56:05 -0800

    Decouple user in app delegate

--- DateMatch/AppDelegate.swift
+++ DateMatch/AppDelegate.swift
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         var initialViewController:UIViewController
         
-        if PFUser.currentUser() != nil {
+        if currentUser() != nil {
             initialViewController = storyboard.instantiateViewControllerWithIdentifier("CardsNavController") as UIViewController
         }
         else {
