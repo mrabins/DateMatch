896dfe2cfd7c7aec7c8f05f44729613f312e1c99
Mark Rabins <markrabins@gmail.com>
2015-11-28 23:38:33 -0800

    Create and inital set up for user struct

---
+++ DateMatch/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  DateMatch
+//
+//  Created by Mark Rabins on 11/28/15.
+//  Copyright (c) 2015 self.swift. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let id: String
+    let pictureURL: String
+    let name: String
+    private let pfUser: PFUser
+}
+
+private func pfUserToUser(user: PFUser) -> User {
+    return User(id: user.objectId, pictureURL: user.objectForKey("picture") as String, name: user.objectForKey("firstName") as String, pfUser: user)
+}
+
+func currentUser() -> User? {
+    if let user = PFUser.currentUser() {
+        return pfUserToUser(user)
+    }
+    return nil
+}
