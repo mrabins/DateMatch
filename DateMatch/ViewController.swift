935a9af6ec6cbc768971d715b23b2c142307ce2e
Mark Rabins <markrabins@gmail.com>
2015-12-06 14:54:23 -0800

    Set ViewControllers in PageViewControllers

--- DateMatch/ViewController.swift
+++ DateMatch/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIPageViewController, UIPageViewControllerDataSource {
+    
+    let cardsVC: UIViewController! = UIStoryboard (name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("CardsNavController") as UIViewController
+    
+    let profileVC: UIViewController! = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ProfileNavController") as UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.whiteColor()
+        dataSource = self
+        
+        self.setViewController([cardsVC], direction: UIPageViewControllerNavigationDirection.Forward, anmited: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Mark: UIPageViewControllerDataSource
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        return nil
+    }
 
 }
 
